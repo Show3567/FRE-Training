c2c517ae0ebbe6e18ec6eb1047c87e3627b0a1c2
show3567 <ydong011@fiu.edu>
2021-03-16 14:52:50 -0400

    update basic java

---
+++ JavaCoreDay1/src/Main.java
@@ -0,0 +1,82 @@
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Principal: ");
+        System.out.println("AnnualInterest: ");
+        System.out.println("Year: ");
+
+        // System.out.println("JDK java development kit");
+        // System.out.println("JRE java runtime environment");
+        // System.out.println("IDE integrated development environment");
+
+        // // type
+        // float floatVal = 1.0F;
+        // double doubleVal = 4.0;
+        // byte byteVal = 7;
+        // short shortVal = 7;
+        // long longValue = 5L;
+        // int intVal = 12;
+        // char charVal = 'a';
+        // boolean boolVal = true;
+        // short res = (short) longValue;
+        // long res = byteVal - longValue;
+        // String str = "10";
+        // int res = Integer.parseInt(str);
+        // System.out.println(res);
+
+        // var date = new Date();
+        // System.out.println(date);
+
+        // Array
+        // var numbers = new int[5];
+        // numbers[0] = 1;
+        // System.out.println(Arrays.toString(numbers));
+
+        // int[][] nums = new int[2][3];
+        // int[] nums = { 1, 2, 3, 4 };
+
+        // for (int num : nums) {
+        // System.out.println(num);
+        // }
+
+        // // const
+        // final int a = 5;
+        // System.out.println("success");
+
+        // String str = new String("hello");
+
+        // String str = "hello";
+        // str += " world";
+
+        // String str1 = "hello";
+        // str1 += " world"; // 'hello' 'hello world' 'hello world'
+
+        // System.out.println(str.equals(str1));
+        // String str = " hello";
+        // System.out.println(str.trim()); // toLowerCase,
+
+        // int a = 6;
+        // String str = String.valueOf(a);
+        // System.out.println(str);
+
+        // Math
+        // int a = (int) (Math.random() * 100);
+        // System.out.println(a);
+
+        // double num = 1231234314.2312131;
+        // var currency = NumberFormat.getCurrencyInstance();
+        // var percentage = NumberFormat.getPercentInstance();
+        // // $123.12
+        // String res = percentage.format(0.1);
+        // System.out.println(res);
+
+        // Scanner scanner = new Scanner(System.in);
+        // int a = scanner.nextInt();
+        // System.out.println("number: " + a);
+    }
+}
